9a1e491c1b9dc0785ad19fe8878b5f2f45b6eaec
ehoga <garthur.san@icloud.com>
2026-06-01 20:46:55 -0300

    feat(avaliacoes): modela entidade de avaliacao

---
+++ backend/src/main/java/com/pji/triagem/model/Classificacao.java
@@ -0,0 +1,7 @@
+package com.pji.triagem.model;
+
+public enum Classificacao {
+    LOW,
+    MOD,
+    HIGH
+}
---
+++ backend/src/main/java/com/pji/triagem/repository/AvaliacaoRepository.java
@@ -0,0 +1,7 @@
+package com.pji.triagem.repository;
+
+import com.pji.triagem.model.Avaliacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
+}
